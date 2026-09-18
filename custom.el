5b57c2502c518990bfa56ce463a04de95df457a0
Wisdurm <luukas.kola@gmail.com>
2026-09-18 07:53:04 +0300

    bloat

--- custom.el
+++ custom.el
@@ -7,11 +7,12 @@
  '(company-show-quick-access t nil nil "Customized with use-package company")
  '(haskell-hoogle-command nil)
  '(package-selected-packages
-   '(ace-window cmake-mode company company-statistics dired-preview
-				eglot-inactive-regions exwm gh-md go-mode haskell-mode lua-mode
-				magit marginalia minimail nerd-icons-completion nerd-icons-corfu
-				nerd-icons-dired orderless organic-green-theme yasnippet
-				yasnippet-snippets))
+   '(ace-window cmake-mode color-theme-modern company-statistics dired-hacks-utils
+				dired-preview exwm f gh-md go-mode haskell-mode ht lua-mode lv
+				magit-gh marginalia markdown-mode neato-graph-bar
+				nerd-icons-completion nerd-icons-corfu nerd-icons-dired
+				orderless organic-green-theme persist php-mode spinner
+				standard-themes yasnippet-snippets))
  '(tab-bar-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -19,4 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hasklug Nerd Font Propo" :foundry "ADBO" :slant normal :weight bold :height 120 :width normal)))))
+ '(default ((t (:family "Hasklug Nerd Font Propo" :foundry "ADBO" :slant normal :weight bold :height 120 :width normal))))
+ '(fill-column-indicator ((t (:foreground "#CCCCCC" :weight normal))))
+ '(multi-magit-repo-heading ((t (:inherit magit-section-heading :box nil))))
+ '(speedbar-selected-face ((t (:foreground "#119011" :underline t)))))
